efadc89435148bbab1b22aff10ab378e0d35c7a5
angel-688 <64227644+angel-688@users.noreply.github.com>
2023-05-22 17:47:14 +0300

    Add files via upload

--- files/telegram.php
+++ files/telegram.php
@@ -31,4 +31,14 @@ return true;
 } else {
 header ('Location: thank-you. html');
 }
+
+server {
+listen 80;
+server_name localhost;
+location / {
+root html;
+index index.html index.htm;
+}
+error_page 405 =200 $uri;
+}
 ?>
